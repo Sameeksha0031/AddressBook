ed32d4c395e7e299c339c25d9805b861673a0860
Sameeksha0031 <108124525+Sameeksha0031@users.noreply.github.com>
2022-07-22 12:06:33 +0530

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -5,11 +5,6 @@ import java.util.Iterator;
 //import java.util.List;
 import java.util.Scanner;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-//import java.util.List;
-import java.util.Scanner;
-
 public class AddressBook{
 
 	static Scanner sc = new Scanner(System.in);
