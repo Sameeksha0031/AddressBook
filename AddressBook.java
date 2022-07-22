b3023df90bef2d200e0f797d1bb81a0cf4d86dc7
Akash <akhvnd@gmail.com>
2022-07-22 11:55:06 +0530

    modified:Create a Contacts in Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,9 +1,54 @@
 package com.AddressBookSystem.util;
 
-public class AddressBook {
+import java.util.ArrayList;
+import java.util.Iterator;
+//import java.util.List;
+import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+//import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook{
+
+	static Scanner sc = new Scanner(System.in);
+	static ArrayList<Contacts> contactlist = new ArrayList<>();
+	
 	public static void main(String[] args) {
+    
+		AddressBook book = new AddressBook();
+		Contacts con = new Contacts(); 
+		
 		System.out.println("Welcome to Address Book");
+		System.out.println();
+		con.setFirstName("Sameeksha");
+		
+		con.setLastName("Vinodiya");
+		
+		con.setAddress("hno 1209 gorakhpur");
+		
+		con.setCity("Jabalpur");
+		
+		con.setState("MP");
+		
+		con.setZip("482001");
+		
+		con.setPhoneNo("9423556900");
+		
+		con.setEmail("Sameeksha@gmail.com");
+		contactlist.add(con);
+		
+	    book.print(contactlist);
+	    System.out.println(" ");
 	}
-
+	
+	public void print( ArrayList<Contacts> contactlist) {
+		
+		for(Contacts contact : contactlist) {
+			
+			System.out.println(contact.toString());
+	    }	
+		
+     }
 }
---
+++ Contacts.java
@@ -0,0 +1,84 @@
+package com.AddressBookSystem.util;
+
+public class Contacts {
+
+	private String firstName, lastName, address, city, state, zip, phoneNo, email ;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String toString() {
+		return "Contacts \n"+
+	           "-----------------------------\n"+
+	           "First Name :\t" + firstName +
+	           "\nLast Name  :\t" + lastName +
+	           "\nAddress    :\t" + address +
+	           "\nCity       :\t" + city +
+	           "\nState      :\t" + state +
+	           "\nZip        :\t" + zip +
+	           "\nPhone No   :\t" + phoneNo+
+	           "\nEmail Id   :\t" + email;
+		
+	}
+}
