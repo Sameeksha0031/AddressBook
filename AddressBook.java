9ef87a30a5cb566776c18ac81215e31acf61d787
Sameeksha0031 <108124525+Sameeksha0031@users.noreply.github.com>
2022-07-25 20:57:00 +0530

    Update AddressBook.java

--- AddressBook.java
+++ AddressBook.java
@@ -13,13 +13,15 @@ public class AddressBook{
 	
 	Scanner sc = new Scanner(System.in);
 	static HashMap<String, AddressBook> map=new HashMap<>();
+	//AddressBook addBook = new AddressBook(); 
+	OperationOnAddressBook book = new OperationOnAddressBook();
 	
 	private int select() {
 		int selection;
 		do {
 			System.out.println();
 	 	    System.out.println("Enter 1. To Add the contacts");
-	    	System.out.println("Enter 2. To Print all contacts");
+	           System.out.println("Enter 2. To Print all contacts");
 		    System.out.println("Enter 3. To Edit the contacts");
 		    System.out.println("Enter 4. To Delete the contacts");
 		    System.out.println("Enter 5. To Exit");
@@ -28,6 +30,25 @@ public class AddressBook{
 		}while(selection<1 || selection > 5);
 			
 		return selection;
+	}
+	
+	int choice;
+	private void showAddressBook() {
+		do {		
+        	choice = select();
+		    switch(choice) {
+		
+		         case 1 :book.add();
+		            break;
+		         case 2 :book.printlist(book.contactlist);
+		            break;
+		         case 3 :book.edit();
+		            break;
+		         case 4: book.delete();
+		            break;
+		         case 5: System.out.println("End");
+		  }
+	   }while(choice != 5);
 	}
 		
 	public static void main(String[] args) {
@@ -43,25 +64,35 @@ public class AddressBook{
 		OperationOnAddressBook book = new OperationOnAddressBook();
 		
 		int choice;
-		System.out.println("Welcome to Address Book");
+		System.out.println("---------------Address Book----------------");
 		System.out.println();
-          do {		
-        	choice = addBook.select();
-		    switch(choice) {
-		
-		         case 1 :book.add();
-		            break;
-		         case 2 :book.printlist(book.contactlist);
-		            break;
-		         case 3 :book.edit();
-		            break;
-		         case 4: book.delete();
-		            break;
-		         case 5: System.out.println("End");
-		  }
-	   }while(choice != 5);
+		while(true) {
+			System.out.println("Enter the 1,2,3 for Address Book and 4 for exist ");
+			Scanner sc = new Scanner(System.in);
+			int option = sc.nextInt();
+			String key=null;
+          switch(option) {
+            case 1: 
+     	            key = "Sameeksha";
+     	            System.out.println("Welcome "+ key);
+     	            addBookOne.showAddressBook();
+     	            break;
+            case 2:
+       	           key = "Ruchi";
+       	           System.out.println("Welcome "+ key);
+       	           addBookOne.showAddressBook();
+       	           break;
+            case 3:
+       	           key = "Krishna";
+       	           System.out.println("Welcome "+ key);
+       	           addBookThree.showAddressBook();
+       	           break;       
+          }	
+          if(option == 4) {
+        	  break;
+          }
         
       }
 		
-	}		
-//}
+    }		
+}
